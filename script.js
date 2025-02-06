8ad40039980474db34dbb694ed4ee635d5045606
wizardsprogramming <wizardsProgramming@gmail.com>
2025-02-05 21:50:42 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Optional: Initialize Materialize components like modals or forms if needed
+document.addEventListener('DOMContentLoaded', function () {
+    M.AutoInit(); // Auto initialize Materialize components
+});
